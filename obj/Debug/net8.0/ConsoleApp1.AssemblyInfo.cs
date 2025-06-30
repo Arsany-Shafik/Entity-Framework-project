c913ca546ac995f3f4881c8b42f3aaf7cbb5c231
Arsany Shafik <arsany.shafik.fci.scu@gmail.com>
2025-06-30 11:30:28 +0300

    create gitignore

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3618a8d876d83aab35c3e8010abbf7f46121921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9f9443dc7bd379256847129aa60a537d830c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
